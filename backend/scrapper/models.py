e92a6f1088d36e8d02a4061877e19a334e77485a
anand <anandvaibhaw000@gmail.com>
2025-02-23 21:57:21 +0530

    updated fields

--- backend/scrapper/models.py
+++ backend/scrapper/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class ScrappedModel(models.Model):
   url = models.URLField()
   content = models.TextField()
-  created_at = models.DateTimeField(auto_add_now=True)
+  created_at = models.DateTimeField(auto_now_add=True)
 
   def __str__(self):
     return self.url
