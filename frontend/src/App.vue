cee1b521b05e065dfb7a06c4d4ea0376857e45c6
anand <anandvaibhaw000@gmail.com>
2025-02-23 22:42:11 +0530

    removed extra divs

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,10 +6,6 @@ import HomeView from "./pages/HomeView.vue"
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HomeView />
-    </div>
   </header>
 
   <RouterView />
