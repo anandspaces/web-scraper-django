a36864a5eac4e7c07314d1970fb944e234be68a4
anand <anandvaibhaw000@gmail.com>
2025-02-23 21:08:56 +0530

    built scrapped model

--- backend/scrapper/models.py
+++ backend/scrapper/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class ScrappedModel(models.Model):
+  url = models.URLField()
+  content = models.TextField()
+  created_at = models.DateTimeField(auto_add_now=True)
+
+  def __str__(self):
+    return self.url
