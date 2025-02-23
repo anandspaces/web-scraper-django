f2534cd68938520f8f4c87eb3e18d0e3beccd664
anand <anandvaibhaw000@gmail.com>
2025-02-23 22:01:21 +0530

    added loader

---
+++ frontend/src/components/Loader.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="loader">Loading...</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Loader'
+})
+</script>
+
+<style scoped>
+.loader {
+  animation: spin 1s linear infinite;
+}
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
+</style>
