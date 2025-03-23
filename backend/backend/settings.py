90d5219982c1719f0a2c6d72dd41b25e2d3852b7
anand <anandvaibhaw000@gmail.com>
2025-03-23 18:05:05 +0530

    fixed name

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'scraper.apps.scraperConfig', # new
+    'scraper.apps.ScraperConfig', # new
     'rest_framework', # new
     'corsheaders', # new
 ]
